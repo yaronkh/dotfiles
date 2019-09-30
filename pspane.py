68c72caad42c3b7ca31a2238133929a4cf081670
Yaron Kahanovitch <ykahanovitch@MLLL4251.magicleap.ds>
2019-09-30 14:20:39 +0300

    tmux: enable running pspane.py from regular terminal

--- pspane.py
+++ pspane.py
@@ -8,18 +8,22 @@ import psutil
 import curses
 
 try:
-    subprocess.check_output(['stty', '-echo'])
+    pid = int(sys.argv[1])
+    if not psutil.pid_exists(pid):
+        raise RuntimeError("process not found")
     curses.setupterm()
     place_begin = curses.tparm(curses.tigetstr("cup"), 0, 0)
     invi =  curses.tparm(curses.tigetstr("civis"))
     printed_tree = ""
-    pid = int(sys.argv[1])
+    subprocess.check_output(['stty', '-echo'])
     while psutil.pid_exists(pid):
       curr_tree = subprocess.check_output(['pstree','-G',str(pid)])[:-1]
       if curr_tree != printed_tree:
-          print"\033c", 
+          subprocess.check_output(['clear'])
           printed_tree = curr_tree
-          print place_begin + printed_tree + invi
+          print (place_begin + printed_tree + invi)
       time.sleep(2)
 finally:
+    subprocess.check_output(['stty', 'echo'])
+    print(curses.tparm(curses.tigetstr("cvvis")))
     pass
