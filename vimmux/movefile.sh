f6e3598c65561e6b6f2d30e3269b7e67c734e7cb
Yaron Kahanovitch <kahayaro@amazon.com>
2021-06-03 16:04:39 +0300

    finish to switch to pyenv virtualenv

--- vimmux/movefile.sh
+++ vimmux/movefile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tmux splitw -p 30 -h -t $1 "source ~/dotfiles/.env/bin/activate && python ~/dotfiles/vimmux/vimmux.py move $1"
+tmux splitw -p 30 -h -t $1 "bash -ic \"pyenv activate nvim > /dev/null 2>&1; exec python ~/dotfiles/vimmux/vimmux.py move $1\""
