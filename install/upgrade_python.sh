027efcf74a51b6cf70cc239ad5c7f1aa9847d03d
Yaron Kahanovitch <kahayaro@amazon.com>
2022-02-20 09:29:58 +0000

    upgrade_python fixes

--- install/upgrade_python.sh
+++ install/upgrade_python.sh
@@ -1,15 +1,27 @@
-#!/bash/bin
+#!/bin/bash
 set -x
 REQ_LOCK=$(mktemp)
 PY_VER=3.9.4
 VNAME=nvim
 pushd ~/dotfiles/
-pyenv install "$PY_VER"
-pyenv activate nvim
+if ! (pyenv versions | grep -q "$PY_VER"); then
+  pyenv install "$PY_VER"
+fi
+
+if ! (pyenv version | grep -q -E "^$VNAME"); then
+  pyenv activate "$VNAME"
+fi
+
 pip freeze | sed  -E 's/==.*//' > "$REQ_LOCK"
-pyenv deactivate "$VNAME"
-pyenv virtualenv-delete "$VNAME"
+pushd ~
+
+if pyenv version | grep -q -E "^$VNAME"; then
+  pyenv deactivate "$VNAME"
+fi
+
+
+echo Y | pyenv virtualenv-delete "$VNAME"
 pyenv virtualenv "$PY_VER" "$VNAME"
 pip install -r "$REQ_LOCK"
-rm "$REQ_LOCK"
+popd
 popd
