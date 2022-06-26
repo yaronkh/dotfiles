44778587e96cf470a4d0620bb0f4da3ee9e35541
Yaron Kahanovitch <kahayaro@amazon.com>
2022-06-26 17:36:07 +0300

    new mechanism to collect python module search path

--- local/nvim.bash
+++ local/nvim.bash
@@ -6,5 +6,12 @@ eval "$(pyenv virtualenv-init -)"
 
 nvim() (
   pyenv activate nvim > /dev/null 2>&1
+  TOP_DIR=$(git rev-parse --show-toplevel 2> /dev/null)
+  if [ -n "$TOP_DIR" ] && [ -d "$TOP_DIR" ] && [ -f "$TOP_DIR/.py_include" ]; then
+    while IFS="" read -r p
+    do
+      export PYTHONPATH="$TOP_DIR/$p:$PYTHONPATH"
+    done < "$TOP_DIR/.py_include"
+  fi
   exec $(which nvim) "$@"
 )
