87f313980ff40fbde509110a7e53a37e653cf5d4
Yaron Kahanovitch <kahayaro@amazon.com>
2022-10-29 18:10:06 +0300

    pspane is presented in popup

--- pspane/pspane.sh
+++ pspane/pspane.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # echo "tmux splitw -p 30 -h -t $1 \"pyenv activate nvim > /dev/null 2>&1 && python ~/dotfiles/pspane/pspane.py $2 $1\"" > /tmp/yaron
-tmux splitw -p 30 -h -t $1 "bash -ic \"pyenv activate nvim > /dev/null 2>&1 ; exec python ~/dotfiles/pspane/pspane.py $2 $1 || sleep 10\""
+# tmux splitw -p 30 -h -t $1 "bash -ic \"pyenv activate nvim > /dev/null 2>&1 ; exec python ~/dotfiles/pspane/pspane.py $2 $1 || sleep 10\""
+tmux  display-popup -t $1 -T "Processes tree of pid $2" "bash -ic \"pyenv activate nvim > /dev/null 2>&1 ; exec python ~/dotfiles/pspane/pspane.py $2 $1 || sleep 10\""
+exit 0
