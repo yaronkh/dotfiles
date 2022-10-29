b6bf3371571134ea46c9e2c3e49b7c36a1ca05ca
Yaron Kahanovitch <kahayaro@amazon.com>
2022-10-29 17:56:17 +0300

    fix pspane for new tmux version
    new tmux version launches bash in non interactive mode,
    and therefore pyenv could not be executed.
    problem solved by explicitly running interactive bash

--- pspane/pspane.sh
+++ pspane/pspane.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # echo "tmux splitw -p 30 -h -t $1 \"pyenv activate nvim > /dev/null 2>&1 && python ~/dotfiles/pspane/pspane.py $2 $1\"" > /tmp/yaron
-tmux splitw -p 30 -h -t $1 "pyenv activate nvim > /dev/null 2>&1 ; python ~/dotfiles/pspane/pspane.py $2 $1"
+tmux splitw -p 30 -h -t $1 "bash -ic \"pyenv activate nvim > /dev/null 2>&1 ; exec python ~/dotfiles/pspane/pspane.py $2 $1 || sleep 10\""
