383ea1e0a0eec365514742ed09aa4f5dcc034cf7
Yaron Kahanovitch <ykahanovitch@nvidia.com>
2025-06-24 23:44:37 +0300

    support install on rhel

--- install_me/install_ubuntu_packages.sh
+++ install_me/install_ubuntu_packages.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# Check if the script is running on an Ubuntu-like system
-if ! grep -qi "Ubuntu" /etc/os-release; then
-  echo "This script only runs on Ubuntu or Ubuntu-like distributions."
-  exit 0
+dir=$( pwd )
+if [ -d "$dir/install_me" ]; then
+    cd "$dir/install_me" || exit 255
 fi
-
-# Check if the file with package names is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <file_with_package_names>"
-  exit 1
-fi
-
-# Read the file and install each package if not already installed
-if ! [ -f /var/lib/apt/periodic/update-success-stamp ]; then
-        echo "please run 'sudo apt-get update'"
-        exit 1
+# check if we have dnf or apt
+if command -v dnf >/dev/null 2>&1; then
+    install_command="dnf install -y"
+    install_file="./packages_rhel.txt"
+    query_command="rpm -qa | grep "
+elif command -v apt-get --version >/dev/null 2>&1; then
+    install_command="apt-get install -y"
+    install_file="./packages_ubuntu.txt"
+    query_command="dpkg -l | grep -v '^rc' | sed -E 's/  */ /g' | cut -d ' ' -f 2 | grep "
+else
+    echo "Unsupported package manager"
+    exit 1
 fi
 
 while IFS= read -r package; do
-  if dpkg -l | grep -q "^ii  $package"; then
+  if eval "$query_command $package" > /dev/null 2>&1; then
     echo "Package $package is already installed."
   else
     echo "Package $package is NOT installed. Installing..."
-    sudo apt-get install -y "$package"
+    eval "sudo $install_command $package"
   fi
-done < "$1"
+done < "$install_file"
