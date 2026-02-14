777c9a8af9ec3419cb25ced394373d7ab9f60b6d
Yaron Kahanovitch <ykahanovitch@nvidia.com>
2026-02-14 18:13:40 +0200

    fastfetch
    Change-Id: I4586385d491fdaf7097dae27bb042e3fd8ab8d58
    Signed-off-by: Yaron <ykahanovitch@nvidia.com>

---
+++ install_me/install_fastfetch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ~/dotfiles/build/bin/
+wget https://github.com/fastfetch-cli/fastfetch/releases/latest/download/fastfetch-linux-amd64.tar.gz
+tar xfpz fastfetch-linux-amd64.tar.gz
+rm fastfetch-linux-amd64.tar.gz
+
